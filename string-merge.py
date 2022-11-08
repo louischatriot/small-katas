a5a5af64f44408e7d16c905767fd3415a6c7a70b
Louis Chatriot <louis.chatriot@gmail.com>
2022-11-08 11:53:55 +0100

    Merge done

--- string-merge.py
+++ string-merge.py
@@ -9,9 +9,9 @@ def is_merge(s, part1, part2):
         if i2 >= len(part2):
             return s[i:] == part1[i1:]
 
-
-
-        if part1[i1] == l:
+        if l == part1[i1] == part2[i2]:
+            return is_merge(s[i+1:], part1[i1+1:], part2[i2:]) or is_merge(s[i+1:], part1[i1:], part2[i2+1:])
+        elif part1[i1] == l:
             i1 += 1
         elif part2[i2] == l:
             i2 += 1
@@ -27,21 +27,10 @@ p1 = "Bahas"
 p2 = "Bananas from am"
 
 
-# res = is_merge(s, p1, p2)
-
-# print(res)
-
-
-
-
-s = "codewars"
-p1 = "code"
-p2 = "wars"
-
-
 res = is_merge(s, p1, p2)
 
 print(res)
+
 
 
 
