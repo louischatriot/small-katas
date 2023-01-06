41a760ee827061c1f1ea46b9e1485d17f9977843
Louis Chatriot <louis.chatriot@gmail.com>
2023-01-06 09:52:12 +0100

    Initial deductions done

--- nonograms.py
+++ nonograms.py
@@ -1,4 +1,8 @@
 from time import time
+import numpy as np
+
+
+
 
 
 def possibilities(clues, N):
@@ -42,19 +46,71 @@ class Nonogram:
         self.grid = [['?' for _ in range(0, self.M)] for _ in range(0, self.N)]
 
 
+    def print(self):
+        print('\n'.join([' '.join(['O' if c == 1 else ('x' if c == 0 else '.') for c in l]) for l in self.grid]))
+        print("========================================================")
+
+
+    def print_clues(self):
+        print("ROW CLUES:", self.rowclues)
+        print("COL CLUES:", self.colclues)
+        print("========================================================")
+
+
     def deduce_initial(self):
         N, M = self.N, self.M
         changed = set()
 
-        for x, clue in enumerate(self.rowclues):
-            # Cannot slide much
-            movable = M - sum(clue) - (len(clue) - 1)
-            for i, c in enumerate(clue):
-                if c > movable:
-                    before = sum(clue[0:i]) + len(clue[0:i])
-                    for dy in range(0, c - movable):
-                        self.grid[x][before + movable + dy] = 1
+        # Cannot slide much
+        # for x, clue in enumerate(self.rowclues):
+            # movable = M - sum(clue) - (len(clue) - 1)
 
+            # if movable == 0:
+                # y = 0
+                # for _ in range(0, clue[0]):
+                    # self.grid[x][y] = 1
+                    # y += 1
+
+                # for c in clue[1:]:
+                    # self.grid[x][y] = 0
+                    # y += 1
+
+                    # for _ in range(0, c):
+                        # self.grid[x][y] = 1
+                        # y += 1
+
+            # else:
+                # for i, c in enumerate(clue):
+                    # if c > movable:
+                        # before = sum(clue[0:i]) + len(clue[0:i])
+                        # for dy in range(0, c - movable):
+                            # self.grid[x][before + movable + dy] = 1
+
+
+        # Cannot slide much
+        for y, clue in enumerate(self.colclues):
+            movable = N - sum(clue) - (len(clue) - 1)
+
+            if movable == 0:
+                x = 0
+                for _ in range(0, clue[0]):
+                    self.grid[x][y] = 1
+                    x += 1
+
+                for c in clue[1:]:
+                    self.grid[x][y] = 0
+                    x += 1
+
+                    for _ in range(0, c):
+                        self.grid[x][y] = 1
+                        x += 1
+
+            else:
+                for i, c in enumerate(clue):
+                    if c > movable:
+                        before = sum(clue[0:i]) + len(clue[0:i])
+                        for dx in range(0, c - movable):
+                            self.grid[before + movable + dx][y] = 1
 
     def solve(self):
         pass
@@ -96,15 +152,14 @@ start = time()
 
 n = Nonogram(clues)
 
-for l in n.grid:
-    print(l)
-print("============================")
+n.print_clues()
+n.print()
 
 n.deduce_initial()
 
-for l in n.grid:
-    print(l)
-print("============================")
+
+n.print()
+
 
 
 
