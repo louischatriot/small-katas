c7774b8b79400492845b60b8b1ff473652fb9f9d
Louis Chatriot <louis.chatriot@gmail.com>
2023-01-29 17:29:57 +0100

    BFS for zeroes done

--- minesweeper.py
+++ minesweeper.py
@@ -31,26 +31,7 @@ class Timer():
 t = Timer()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-gamemap = """
+big_map = """
 ? ? 0 ? ? ? 0 0 ? ? ? 0 0 0 0 ? ? ? 0
 ? ? 0 ? ? ? 0 0 ? ? ? 0 0 0 0 ? ? ? ?
 ? ? 0 ? ? ? ? ? ? ? ? 0 0 0 0 ? ? ? ?
@@ -76,7 +57,8 @@ gamemap = """
 0 0 0 0 ? ? ? ? ? ? ? ? ? ? 0 ? ? ? ?
 """.strip()
 
-result = """
+
+big_res = """
 1 1 0 1 1 1 0 0 1 1 1 0 0 0 0 1 1 1 0
 x 1 0 1 x 1 0 0 2 x 2 0 0 0 0 1 x 2 1
 1 1 0 2 3 3 1 1 3 x 2 0 0 0 0 1 2 x 1
@@ -103,21 +85,7 @@ x 1 0 1 x 1 0 0 2 x 2 0 0 0 0 1 x 2 1
 """.strip()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-gamemap = """
+small_map = """
 ? ? ? ? ? ?
 ? ? ? ? ? ?
 ? ? ? 0 ? ?
@@ -127,7 +95,7 @@ gamemap = """
 """.strip()
 
 
-result = """
+small_res = """
 1 x 1 1 x 1
 2 2 2 1 2 2
 2 x 2 0 1 x
@@ -138,9 +106,93 @@ result = """
 
 
 
+map = small_map
+res = small_res
+solution = [[int(c) if c != 'x' else 'x' for c in l.split(' ')] for l in res.split('\n')]
+n_mines = sum([sum([1 if c == 'x' else 0 for c in l]) for l in solution])
 
 
-print(gamemap)
+deltas = [(dx, dy) for dx in [-1, 0, 1] for dy in [-1, 0, 1] if dx != 0 or dy != 0]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def open(x, y):
+    if solution is not None:
+        if solution[x][y] == 'x':
+            raise ValueError("Boom")
+        else:
+            return solution[x][y]
+
+
+
+
+
+def solve_mine(map, n):
+    map = [[int(c) if c != '?' else '?' for c in l.split(' ')] for l in map.split('\n')]
+    N, M = len(map), len(map[0])
+
+    print("=================================")
+    print('\n'.join([' '.join([str(c) for c in l]) for l in map]))
+
+    todo_z = list()
+    todo = list()
+
+    # Assuming we never get anything else than zeroes as hints initially
+    for x in range(0, N):
+        for y in range(0, M):
+            if map[x][y] == 0:
+                todo_z.append((x, y))
+
+    while len(todo_z) > 0:
+        x, y = todo_z.pop(0)
+        for dx, dy in deltas:
+            if 0 <= x + dx < N and 0 <= y + dy < M and map[x + dx][y + dy] == '?':
+                v = open(x + dx, y + dy)
+                map[x + dx][y + dy] = v
+                if v == 0:
+                    todo_z.append((x + dx, y + dy))
+                else:
+                    todo.append((x + dx, y + dy))
+
+
+    print("=================================")
+    print('\n'.join([' '.join([str(c) for c in l]) for l in map]))
+
+
+
+
+
+
+
+
+
+
+
+t.reset()
+
+solve_mine(map, n_mines)
+
+t.time("Algo done")
+
+
+
+
+
+
+
+
+
 
 
 
